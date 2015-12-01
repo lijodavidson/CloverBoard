9f7790e5fac3319f64436cc986fe7bf0ce6adb5d
lijodavidson <lijodavidson@hotmail.com>
2015-12-01 11:30:18 +0530

    second com

--- app/src/main/java/com/example/lijo/cloverboard/MainActivity.java
+++ app/src/main/java/com/example/lijo/cloverboard/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity  {
 
         setupNavigationDrawerContent(navigationView);
         //displays first in nav view
+
         setFragment(0);
 
     }
