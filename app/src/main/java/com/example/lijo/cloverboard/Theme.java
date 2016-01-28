0da574258392d5b3d0ecb106ebc36552cae1dece
lijodavidson <lijodavidson@hotmail.com>
2016-01-28 17:33:52 +0530

    28th evening with background changes, changed color from white to light grey ! and started wih  themes

--- app/src/main/java/com/example/lijo/cloverboard/Theme.java
+++ app/src/main/java/com/example/lijo/cloverboard/Theme.java
@@ -1,16 +1,48 @@
 package com.example.lijo.cloverboard;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class Theme extends AppCompatActivity {
-
+    private GridView gridViewObj;
+    private ArrayList<ClipData.Item> themeArray = new ArrayList<>();
+    private ArrayList<Integer> dialogArray = new ArrayList<>();
+    /*private ThemesAdapter themesAdapter;
+    private DialogImagesAdapter dialogImagesAdapter;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        //set Main grid view item
+      /*  themeArray.add(new ThemeItems(R.drawable.morning, "Evening"));
+        themeArray.add(new ThemeItems(R.drawable.re, "Morning"));
+        themeArray.add(new ThemeItems(R.drawable.morning, "Night"));
+
+        //set Dialog grid view item
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.re);
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.re);
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.re);
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.re);
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.morning);
+        dialogArray.add(R.drawable.re);
+        dialogArray.add(R.drawable.morning);
+*/
+
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/lijo/cloverboard/ThemeItems.java
@@ -0,0 +1,18 @@
+package com.example.lijo.cloverboard;
+
+
+class ThemeItems {
+    int image;
+    String title;
+    public ThemeItems(int image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+    public int getImage() {
+        return image;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
